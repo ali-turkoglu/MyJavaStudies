b472d0047133dc3fa8be5ac317266efd53981780
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-02 17:23:59 +0300

    adding week12 live class

---
+++ liveReviewSessions/week12/Employee.java
@@ -0,0 +1,17 @@
+package week12;
+
+public class Employee {
+
+    public String name, jobTitle;
+    public int ID;
+    public double salary;
+
+    public Employee(String name, String jobTitle, int ID, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.salary = salary;
+    }
+
+
+}
