f95e94ded2c5e7fef32995cf32174c01daa90a7d
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-17 13:44:12 +0300

    day41 MultiCatchBlocks updated

---
+++ src/day41_Exceptions/FinallyBlock.java
@@ -0,0 +1,22 @@
+package day41_Exceptions;
+
+public class FinallyBlock {
+
+    public static void main(String[] args) {
+
+        int [] arr={1,2,3};
+
+        try {
+            System.out.println(arr[10]);
+            System.out.println("try block");
+        }catch (RuntimeException e){
+            System.out.println("catch block");
+            e.printStackTrace();
+            System.exit(0);
+        }finally {
+            System.out.println("finally block");
+        }
+
+
+    }
+}
