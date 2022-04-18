18b17943e8cdbebeb5bca98215e36ca899a91d59
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-18 16:14:46 +0300

    day42 disadventage of throws keyword

--- src/day42_Exceptions/DisadventageOfThrowsKeyword.java
+++ src/day42_Exceptions/DisadventageOfThrowsKeyword.java
@@ -1,15 +1,49 @@
 package day42_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class DisadventageOfThrowsKeyword {
 
-    public void method1()throws InterruptedException{
+
+
+
+    public static void method1(){
+
+        // method1();  ----> buradan çağırdığımızda error veriyor.
+        // method3();   --->  "          "
+        method4();  //  ---> try & catch ile error çözüldüğü için hata vermiyor
+    }
+
+
+    public static void method2()throws InterruptedException{
         System.out.println("Hello");
         Thread.sleep(3000);
         System.out.println("World");
     }
 
-    public void method2(){
-       // method1();  ----> buradan çağırdığımızda error veriyor.
+    public static void method3() throws FileNotFoundException {
+        new FileInputStream("");
     }
+
+    public static void method4(){
+        System.out.println("Test started");
+        try {
+            new FileInputStream("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Test completed");
+    }
+
+    public static void main(String[] args) {
+
+       // method2();  //----> buradan çağırdığımızda error veriyor.
+       // method3();  // --->  "          "
+        method4();  //  ---> try & catch ile error çözüldüğü için hata vermiyor
+
+    }
+
+
 
 }
