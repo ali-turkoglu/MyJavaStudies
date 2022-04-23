1ebf0d5b2d8678f1d336a04a59965b7e49799c67
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-23 12:09:35 +0300

    day43 Abstract

---
+++ src/day43_OOP_Abstraction/car/Car.java
@@ -0,0 +1,65 @@
+package day43_OOP_Abstraction.car;
+
+public abstract class Car {
+    private  final String make,model;
+    private String color;
+    private final int year;
+    private double price;
+
+    public Car(String make, String model, String color, int year, double price) {
+        this.model=model;
+        this.make=make;
+        setColor(color);
+        if (year<1886){
+            throw new RuntimeException("Invalid Year: "+year);
+        }
+        this.year=year;
+        setPrice(price);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if(price <= 0){
+            throw new RuntimeException("Invalid price: "+price);
+        }
+        this.price = price;
+    }
+
+    public abstract void start();
+    public abstract void stop();
+
+
+}
+/*
+Car Task:
+	abstract Car:
+		make, model, color, year, price....
+		start();
+		stop()
+ */
