762ec10a6882ee691939001c269db2e67b18523f
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-24 14:55:14 +0300

    day43 Shape and Employee tasks finished

--- src/day43_OOP_Abstraction/employee/Amazon.java
+++ src/day43_OOP_Abstraction/employee/Amazon.java
@@ -7,8 +7,8 @@ public class Amazon {
 
 
         Employee t1=new Tester("Ahmet",33,'M',23421,"QA",99999);
-        Employee d1=new Developer("Ali",33,'M',234663,"Java",99999);
-        Employee teacher1=new Teacher("Filiz",33,'F',23443,"Math",99999);
+        Employee d1=new Developer("Ali",33,'M',234663,"Java developer",99999);
+        Employee teacher1=new Teacher("Filiz",33,'F',23443,"Math teacher",99999);
         Employee driver1=new Driver("mehmet",33,'M',21111,"Lyft Driver",99999);
 
 
@@ -16,6 +16,14 @@ public class Amazon {
         System.out.println(d1);
         System.out.println(teacher1);
         System.out.println(driver1);
+
+        System.out.println("----------------------------------------");
+
+        t1.work();
+        t1.sleep();
+        d1.work();
+        driver1.work();
+        teacher1.work();
 
 
     }
