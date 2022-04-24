c6255975ece527506139c663685bffccaa20b8b2
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-24 14:49:34 +0300

    day43 Shape and Employee tasks finished

--- src/day43_OOP_Abstraction/employee/Amazon.java
+++ src/day43_OOP_Abstraction/employee/Amazon.java
@@ -4,10 +4,12 @@ public class Amazon {
 
     public static void main(String[] args) {
 
-        Employee t1=new Tester("Ahmet",33,'M',23423,"QA",99999);
-        Employee d1=new Developer("Ali",33,'M',23423,"Java",99999);
-        Employee teacher1=new Teacher("Filiz",33,'F',23423,"QA",99999);
-        Employee driver1=new Tester("mehmet",33,'M',23423,"QA",99999);
+
+
+        Employee t1=new Tester("Ahmet",33,'M',23421,"QA",99999);
+        Employee d1=new Developer("Ali",33,'M',234663,"Java",99999);
+        Employee teacher1=new Teacher("Filiz",33,'F',23443,"Math",99999);
+        Employee driver1=new Driver("mehmet",33,'M',21111,"Lyft Driver",99999);
 
 
         System.out.println(t1);
--- src/day43_OOP_Abstraction/employee/Tester.java
+++ src/day43_OOP_Abstraction/employee/Tester.java
@@ -1,6 +1,6 @@
 package day43_OOP_Abstraction.employee;
 
-public class Tester extends Employee{
+public final class Tester extends Employee{
 
     public Tester(String name, int age, char gender, int id, String jobTitle, int salary) {
         super(name, age, gender, id, jobTitle, salary);
