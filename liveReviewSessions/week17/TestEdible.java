6947a5146b087d584fbe44a83c684c6ae8133e42
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-07 21:26:43 +0300

    week17 Abstract and Interface

---
+++ liveReviewSessions/week17/TestEdible.java
@@ -0,0 +1,64 @@
+package week17;
+
+public class TestEdible {
+
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+    interface Edible{
+        // Describes how to eat
+        public abstract String howToEat(); // public abstract is redundant
+    }
+
+    abstract class Animal{
+        // return animal sound
+        public abstract String sound();
+    }
+
+    class Chicken extends Animal implements Edible{
+
+        @Override
+        public String howToEat() {
+            return "Chicken: Fry it";
+        }
+
+        @Override
+        public String sound() {
+            return "Chicken: cock-a-doodle-doo";
+        }
+
+        class Tiger extends Animal{
+
+            @Override
+            public String sound() {
+                return null;
+            }
+        }
+
+        abstract class Fruit implements Edible{
+
+        }
+
+        class Apple extends Fruit{
+
+            @Override
+            public String howToEat() {
+                return "Apple: make apple cider";
+            }
+        }
+
+        class Orange extends Fruit{
+
+            @Override
+            public String howToEat() {
+                return null;
+            }
+        }
+
+
+    }
+
+
